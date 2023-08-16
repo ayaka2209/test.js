8f72d0047452090dfe5ae23f461418dd906e14a7
ayaka tooya <na081543@icloud.com>
2023-08-16 10:05:30 +0900

    テスト

---
+++ js../switch.js
@@ -0,0 +1,14 @@
+const season = "夏";
+switch (season) {
+  case "春":
+    console.log("花見に行きます");
+    break;
+  case "夏":
+    console.log("プールに行きます");
+    break;
+  case "秋":
+    console.log("栗を拾います");
+    break;
+  default:
+    console.log("勉強をします");
+}
--- js../test.js
+++ js../test.js
@@ -1,7 +1,16 @@
-const hasStudentCard = true
+const userAge = 26;
+const hasStudentCard = false;
+const minAge = 3;
+const maxAge = 25;
+
+
 // 定数を定義した　定数にtrueを代入
 // === aとbが等しい場合にtrueを返す
 if (hasStudentCard === true) {
   console.log("学割適用");
+} else if (userAge >= minAge && userAge <= maxAge) {
+  console.log("学割適用");
+} else {
+  console.log("条件に合わないため割引できません");
 }
 //　もし学生証を持っていれば”学割適用"がコンソールに出力される
